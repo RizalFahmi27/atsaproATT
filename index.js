e6d928ca01e1af33101259f406c4a0a335723bc1
Rizal Fahmi <rizalfahmi27@gmail.com>
2017-05-08 12:52:00 +0700

    Port changed

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var io = require('socket.io')(http);
 var clients = {};
 var rooms = [];
 
-var server_port = 	process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_port = 3030;
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -101,6 +102,6 @@ nsp.on('connection', function(socket){
 
 
 
-http.listen(server_port,server_ip_address, function(){
+http.listen(server_port, function(){
 	 console.log( "Listening on " + server_ip_address + ", port " + server_port );
 });
