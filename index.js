50407c2ebaaf1a996c67b4e2556d12842427b99b
Rizal Fahmi <rizalfahmi27@gmail.com>
2017-07-11 10:47:46 +0700

    Delete room fix

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var clients = [];
 var rooms = [];
 var roomIndex = {};
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 // var server_port = 3000;
-// var server_port = process.env.PORT || 8000
+var server_port = process.env.PORT || 8000
 //var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 //
 app.get('/', function(req, res){
